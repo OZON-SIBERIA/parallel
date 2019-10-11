f904b07c2922d1cc6c7f97a2f602f8a000345f33
OZON-SIBERIA <medvedev152@live.com>
2019-10-11 13:30:40 +1000

    second

--- OpenMP_Project/Main.cpp
+++ OpenMP_Project/Main.cpp
@@ -3,6 +3,9 @@
 #include <thread>
 #include <stdio.h>
 #include <chrono>
+#include <thread>
+#include <vector>
+#include <mutex>
 #define steps 100000
 
 double f(double x)
@@ -10,57 +13,66 @@ double f(double x)
 	return 4 / (1 + x * x);
 }
 
-double integral_OMP(double a, double b) 
+/*double integral_OMP(double a, double b) 
 {
 	int T = omp_get_num_procs(); //количество доступных потоков
 	double res = 0;
 	double dx = (b - a) / steps;
-#pragma omp_parallel
+#pragma omp parallel
 	{
 		int t = omp_get_thread_num(); //индекс потока
 		double r = 0;
-		int i = 0;
-		for (i; i < steps; i += T)
+		for (int i = 1; i < steps; i += T)
 			r += f(dx*(i + 0.5) + a);
-#pragma omp_critical
+#pragma omp critical
 		{
 			res += r;
 		}
 	}
 	return res * dx;
-}
+}*/
 
-/*double integral_CPP(double a, double b)
+double integral_CPP(double a, double b)
 {
 	auto T = std::thread::hardware_concurrency(); //количество доступных потоков
 	double dx = (b - a) / steps;
+	std::mutex mtx;
 	std::vector<std::thread> threads;
 	double res = 0;
+	double r = 0;
 	for (unsigned t = 0; t < T; t++)
 	{
-		threads.emplace_back(std::thread([T, t, dx, &res, a]()
+		threads.emplace_back(std::thread([T, t, dx, &res, &mtx, a, &r]()
 			{
 				for (auto i = t; i < steps; i += T)
 				{
-					res += f(dx * (i + 0.5) + a);
+					r += f(dx * (i + 0.5) + a);
 				}
 			}));
-		for (auto& thr : threads)
-		{
-			thr.join();
-			return res * dx;
-		}
-
+		mtx.lock();
+		res += r;
+		mtx.unlock();
 	}
-}*/
+	return res;
+}
 
 
-int main(int argc, char**argv)
+/*int main(int argc, char**argv)
 {
-	unsigned t0 = omp_get_wtime();
+	double t0 = omp_get_wtime();
 	double result_omp = integral_OMP(0, 1);
 	double t1 = omp_get_wtime();
-	printf("result: %g, time: %g.ms \n", result_omp, ((t1-10)*1000));
+	printf("result: %g, time: %g.ms \n", result_omp, ((t1-t0)*1000));
+	return 0;
+
+}*/
+
+int main(int argc, char** argv)
+{
+	double t0 = omp_get_wtime();
+	double result_CPP = integral_CPP(0, 1);
+	double t1 = omp_get_wtime();
+	printf("result: %g, time: %g.ms \n", result_CPP, ((t1 - t0) * 1000));
 	return 0;
 
 }
